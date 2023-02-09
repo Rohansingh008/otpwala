62038fa8177feeafa3deb167a44583a40cd0558b
rohan singh <124667804+Rohansingh008@users.noreply.github.com>
2023-02-09 22:00:46 +0530

    Add files via upload

---
+++ cred.py
@@ -0,0 +1,22 @@
+#Twilio Details
+
+account_sid = 'AC30aa5c38670579fc1860bbd37a166f47'
+auth_token = 'a83b6d83818666238f47cccb2cc7c164'
+twilionumber = '+12202721989'
+twiliosmsnumber = '+12202721989'
+
+#FC Bot
+API_TOKEN = "5939311107:AAGo34F_PHDrFA7F7GcUu3Cm_wTzfKb3_0o"
+
+#Host URL
+callurl = 'https://e789-8-219-3-64.ap.ngrok.io/'
+twiliosmsurl = 'https://e789-8-219-3-64.ap.ngrok.io/sms'
+
+
+
+
+
+
+
+
+
